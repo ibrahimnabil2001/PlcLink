580e707d0d43cdf6914ae4948f34fc5ac603ba96
ibrahimnabil2001 <32104350+ibrahimnabil2001@users.noreply.github.com>
2019-12-29 09:00:12 +0200

    Update MainActivity.java

--- app/src/main/java/org/systems/bluelink/plclink/MainActivity.java
+++ app/src/main/java/org/systems/bluelink/plclink/MainActivity.java
@@ -6,6 +6,7 @@ import android.bluetooth.BluetoothSocket;
 import android.content.Intent;
 import android.os.Handler;
 import android.os.Message;
+
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.DialogFragment;
