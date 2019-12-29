823857ed24fd473f9918b1ca345771673cd3fde8
ibrahimnabil2001 <32104350+ibrahimnabil2001@users.noreply.github.com>
2019-12-29 08:52:22 +0200

    added new project dialog

--- app/src/main/java/org/systems/bluelink/plclink/MainActivity.java
+++ app/src/main/java/org/systems/bluelink/plclink/MainActivity.java
@@ -6,7 +6,6 @@ import android.bluetooth.BluetoothSocket;
 import android.content.Intent;
 import android.os.Handler;
 import android.os.Message;
-
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.DialogFragment;
